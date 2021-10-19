c894885192d384be333b3f701dd7e1047dc7ab19
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-10-19 12:06:05 -0400

    cleanup

--- pyautogui/src/credentials.py
+++ pyautogui/src/credentials.py
@@ -1,12 +1,8 @@
 import praw
 
-#Reddit developer credentials
-reddit=praw.Reddit(client_id='', 
-                    client_secret='', 
-                    username='', 
-                    password='', 
-                    user_agent='')
+# Reddit developer credentials
+reddit = praw.Reddit(client_id="", client_secret="", username="", password="", user_agent="")
 
-#Instagram password and username
-IGusername = ''
-IGpassword = ''
+# Instagram password and username
+IGusername = ""
+IGpassword = ""
